c24860f45e914847a7d399e4fe3c93207b3be6f0
Axel BLEUSE <axel.bleuse@orange.fr>
2021-03-17 10:24:02 +0100

    refactor text file

--- run.py
+++ run.py
@@ -18,12 +18,12 @@ def get_all_users():
     result = wrapper.get_all_users()
     if result:
         return jsonify(status="True",
-                   result=[
-                       {"nom": user.nom,
-                        "prenom": user.prenom,
-                        "email": user.email,
-                        "ville": user.ville,
-                        "telephone": user.telephone} for user in result.all()])
+                       result=[
+                           {"nom": user.nom,
+                            "prenom": user.prenom,
+                            "email": user.email,
+                            "ville": user.ville,
+                            "telephone": user.telephone} for user in result.all()])
     return jsonify(status="False")
 
 
@@ -31,15 +31,14 @@ def get_all_users():
 def get_user(email):
     result = wrapper.get_user_by_id(email)
 
-
     if result:
         return jsonify(status="True",
-                   result={"nom": result.nom,
-                           "prenom": result.prenom,
-                           "email": result.email,
-                           "ville": result.ville,
-                           "telephone": result.telephone}
-                   )
+                       result={"nom": result.nom,
+                               "prenom": result.prenom,
+                               "email": result.email,
+                               "ville": result.ville,
+                               "telephone": result.telephone}
+                       )
     return jsonify(status="False")
 
 
@@ -47,17 +46,16 @@ def get_user(email):
 def mofify_user(email):
     result = wrapper.update_attribute(email, request.form.to_dict())
 
-
     if result:
         return jsonify(status="True",
-                   message="updated",
-                   result={
-                       "nom": result.nom,
-                       "prenom": result.prenom,
-                       "email": result.email,
-                       "ville": result.ville,
-                       "telephone": result.telephone}
-                   )
+                       message="updated",
+                       result={
+                           "nom": result.nom,
+                           "prenom": result.prenom,
+                           "email": result.email,
+                           "ville": result.ville,
+                           "telephone": result.telephone}
+                       )
     return jsonify(status="False")
 
 
@@ -66,9 +64,9 @@ def delete_user(email):
     result = wrapper.delete_user_by_id(email)
     if result:
         return jsonify(status="True",
-                   message="Deleted",
-                   email=email
-                   )
+                       message="Deleted",
+                       email=email
+                       )
     return jsonify(status="False")
 
 
