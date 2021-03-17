7793c956be1e341499a9ef770da3a82602d5b2b8
Axel BLEUSE <axel.bleuse@orange.fr>
2021-03-17 11:35:21 +0100

    rektification

--- wrapper.py
+++ wrapper.py
@@ -1,3 +1,6 @@
+# coding: utf-8
+
+
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, Column, String, ForeignKey
