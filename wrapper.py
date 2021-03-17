4629a8c030d25aea17cea0e722e9569ccab8de81
Axel BLEUSE <axel.bleuse@orange.fr>
2021-03-17 14:57:01 +0100

    modifying mysql host

--- wrapper.py
+++ wrapper.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, Column, String, ForeignKey
 
-MYSQL_HOST = "localhost"
+MYSQL_HOST = "0.0.0.0"
 MYSQL_PORT = 3306
 MYSQL_USER = "root"
 MYSQL_PWD = "secret"
