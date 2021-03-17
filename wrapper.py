8ab085a9f69bd99b3bc85adbe109657baa785f67
Axel BLEUSE <axel.bleuse@orange.fr>
2021-03-17 13:25:43 +0100

    new run

--- wrapper.py
+++ wrapper.py
@@ -25,45 +25,6 @@ session = Session()
 Base = declarative_base()
 
 
-class User(Base):
-    """
-    Cette classe correspond à la version objet de notre table user
-    """
-
-
-__tablename__ = "user"
-
-email = Column(String(120), unique=True, nullable=False, primary_key=True)
-nom = Column(String(80), nullable=False)
-prenom = Column(String(80), nullable=False)
-telephone = Column(String(80), nullable=False)
-
-
-def add_user(email, nom, prenom, telephone):
-    try:
-        user = User(email=email,
-                    nom=nom,
-                    prenom=prenom,
-                    telephone=telephone)
-
-        session.add(user)
-        session.commit()
-        return True
-
-    except Exception as e:
-        print(e)
-        return False
-
-
-def get_user_by_id(email):
-    try:
-        result = session.query(User).filter_by(email=email).first()
-        return result
-    except Exception as e:
-        print(e)
-        return False
-
-
 def get_all_users():
     try:
         result = session.query(User).filter_by()
@@ -73,30 +34,62 @@ def get_all_users():
         return False
 
 
-def delete_user_by_id(email):
-    try:
-        user_to_delete = get_user_by_id(email)
-        if user_to_delete:
-            session.delete(user_to_delete)
+class User(Base):
+    """
+    Cette classe correspond à la version objet de notre table user
+    """
+    __tablename__ = 'user'
+    email = Column(String(120), unique=True, nullable=False, primary_key=True)
+    nom = Column(String(80), nullable=False)
+    prenom = Column(String(80), nullable=False)
+    telephone = Column(String(80), nullable=False)
+
+    def add_user(email, nom, prenom, telephone):
+        try:
+            user = User(email=email,
+                        nom=nom,
+                        prenom=prenom,
+                        telephone=telephone)
+
+            session.add(user)
             session.commit()
             return True
-        else:
+
+        except Exception as e:
+            print(e)
             return False
-    except Exception as e:
-        print(e)
-        return False
 
+    def get_user_by_id(self):
+        try:
+            result = session.query(User).filter_by(email=self).first()
+            return result
+        except Exception as e:
+            print(e)
+            return False
 
-def update_attribute(email, attributes):
-    try:
-        user_to_update = get_user_by_id(email)
-        if user_to_update:
-            for k, v in attributes.items():
-                setattr(user_to_update, k, v)
+    def delete_user_by_id(self):
+        try:
+            user_to_delete = self.get_user_by_id(self.email)
+            if user_to_delete:
+                session.delete(user_to_delete)
                 session.commit()
-            return user_to_update
-        else:
+                return True
+            else:
+                return False
+        except Exception as e:
+            print(e)
             return False
-    except Exception as e:
-        print(e)
-        return False
+
+    def update_attribute(self, attributes):
+        try:
+            user_to_update = self.get_user_by_id(self)
+            if user_to_update:
+                for k, v in attributes.items():
+                    setattr(user_to_update, k, v)
+                    session.commit()
+                return user_to_update
+            else:
+                return False
+        except Exception as e:
+            print(e)
+            return False
